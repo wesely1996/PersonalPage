b8dd14124f111139a440f678bdbdaae8af8a2157
Nikola <nikola.wesely.96@gmail.com>
2022-03-14 23:28:25 +0100

    Profile and removed sound

--- functions/index_functions.js
+++ functions/index_functions.js
@@ -3,22 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('loading').classList.add("hide");
  }, false);
 
-var bgmusic = document.getElementById("bgmusic");
-bgmusic.volume = 0.2;
-
-function playMusic() {
-    let mb = document.getElementById("musicImg");
-    if(mb.alt == "🔊"){
-        mb.src = "./images/sound_off.png";
-        mb.alt = "🔈";
-        bgmusic.pause();
-    }else{
-        mb.src = "./images/sound_on.PNG";
-        mb.alt = "🔊";
-        bgmusic.play();
-    }
-}
-
 function showContacts(){
     let contacts = document.getElementById("contactBubble");
     contacts.innerHTML = 
