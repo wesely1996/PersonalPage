00d5ef04d05f26b43d203d3a0876ef578e600acb
wesely1996 <nikola.wesely.96@gmail.com>
2025-06-08 22:50:16 +0200

    frame loading for codex

--- src/app/components/codex/codex.component.ts
+++ src/app/components/codex/codex.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PdfViewerComponent } from '../pdf-viewer/pdf-viewer.component';
 
 @Component({
@@ -7,4 +7,17 @@ import { PdfViewerComponent } from '../pdf-viewer/pdf-viewer.component';
   styleUrl: './codex.component.scss',
   imports: [PdfViewerComponent],
 })
-export class CodexComponent {}
+export class CodexComponent implements OnInit {
+  grid: { delay: number }[] = [];
+  rows = 10;
+  cols = 10;
+  totalDuration = 2000;
+
+  ngOnInit(): void {
+    const boxCount = this.rows * this.cols;
+    for (let i = 0; i < boxCount; i++) {
+      const delay = (i / boxCount) * this.totalDuration;
+      this.grid.push({ delay });
+    }
+  }
+}
